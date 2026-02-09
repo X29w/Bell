b983b2590fe406b674f48be23f396b2bbc22a40e
園洛樱介 <1937009360@qq.com>
2026-02-09 15:53:14 +0800

    ``` chore(project): add node_modules to gitignore and format JSON files
    - Add node_modules to .gitignore file
    - Format back-end/nest-cli.json with consistent indentation
    - Format back-end/package.json with consistent indentation
    - Format back-end/tsconfig files with consistent indentation
    - Format front-end package.json with consistent indentation
    - Update front-end config files with consistent formatting
    - Format TypeScript files across both front-end and back-end projects
    - Standardize import statements and object property formatting
    ```

--- front-end/babel.config.js
+++ front-end/babel.config.js
@@ -1,11 +1,14 @@
 // babel-preset-taro 更多选项和默认值：
 // https://docs.taro.zone/docs/next/babel-config
 module.exports = {
-  presets: [
-    ['taro', {
-      framework: 'react',
-      ts: true,
-      compiler: 'vite',
-    }]
-  ]
-}
+	presets: [
+		[
+			"taro",
+			{
+				framework: "react",
+				ts: true,
+				compiler: "vite",
+			},
+		],
+	],
+};
--- front-end/src/pages/index/index.tsx
+++ front-end/src/pages/index/index.tsx
@@ -5,17 +5,17 @@ import type { FC } from "react";
 interface IndexProps {}
 
 const Index: FC<IndexProps> = () => {
-  useLoad(() => {
-    console.log("Page loaded.");
-  });
-  return (
-    <View className="index">
-      <Text>Hello world!</Text>
-      <Button>按钮文案</Button>
-      <Button type="primary">按钮文案</Button>
-      <Button type="warn">按钮文案</Button>
-    </View>
-  );
+	useLoad(() => {
+		console.log("Page loaded.");
+	});
+	return (
+		<View className="index">
+			<Text>Hello world!</Text>
+			<Button>按钮文案</Button>
+			<Button type="primary">按钮文案</Button>
+			<Button type="warn">按钮文案</Button>
+		</View>
+	);
 };
 
 export default Index;
